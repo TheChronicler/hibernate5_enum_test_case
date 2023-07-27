5b8c739131bebb7a2888da6516e281e4bb20643a
Clemens Rüttermann <clemens.ruettermann-extern@eucon.com>
2023-07-27 11:42:11 +0200

    Add test code

---
+++ src/test/java/org/hibernate/bugs/TestTbl.java
@@ -0,0 +1,15 @@
+package org.hibernate.bugs;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "test")
+public class TestTbl {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "test_enum", length = 10)
+	@Enumerated(EnumType.STRING)
+	private TestEnum testEnum;
+}
